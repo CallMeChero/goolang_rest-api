6c9e32f428f09130d0abd0593539bf7c3a0437d6
CallMeChero <david.ceranic@gmail.com>
2025-03-12 19:12:53 +0100

    Resolving signup of users

---
+++ routes/users.go
@@ -0,0 +1,26 @@
+package routes
+
+import (
+	"example/rest-api/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func signup(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data"})
+		return
+	}
+
+	err = user.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to signup user"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "User signed-up"})
+}
