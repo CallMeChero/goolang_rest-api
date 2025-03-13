44b7c62d93bf0f1dd2f7c6f3cdaf9c265a8983cf
CallMeChero <david.ceranic@gmail.com>
2025-03-13 11:52:25 +0100

    Resolve bug regarding user pointer and add protected routes

--- routes/events.go
+++ routes/events.go
@@ -18,7 +18,7 @@ func getEvents(context *gin.Context) {
 		return
 	}
 
-	err := utils.VerifyToken(token)
+	_, err := utils.VerifyToken(token)
 	if err != nil {
 		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
 		return
@@ -47,18 +47,31 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+		return
+	}
+
 	var event models.Event
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data"})
 		return
 	}
 
-	event.ID = 1
-	event.UserID = 1
-
+	event.UserID = userId
 	err = event.Save()
+
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to create event"})
 		return
